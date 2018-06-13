5b21dbd18b247c60a9957a81215bc5cced01478d
Avaer Kazmer <a@modules.io>
2018-06-13 19:00:39 -0400

    Major rewrite to inherit from Array

--- classlist.js
+++ classlist.js
@@ -1,121 +1,90 @@
-'use strict';
+const trim = require('trim');
 
-module.exports = ClassList
+class ClassList extends Array {
+  constructor(className, onchange) {
+    super();
 
-var indexOf = require('component-indexof'),
-    trim = require('trim'),
-    arr = Array.prototype
+    this.reset(className);
+    this.onchange = onchange;
+  }
+  
+  item(k) {
+    const v = this[k];
+    return v !== undefined ? v : null;
+  }
+  
+  reset(className) {
+    var classes = trim(className).split(/\s+/), i;
+    this.length = 0;
 
-/**
- * ClassList(elem) is kind of like Element#classList.
- *
- * @param {Element} elem
- * @return {ClassList}
- */
-function ClassList (className, onchange) {
-  if (!(this instanceof ClassList))
-    return new ClassList(className)
+    for (i = 0; i < classes.length; i += 1) {
+      if (classes[i]) {
+        this.push(classes[i]);
+      }
+    }
+  }
+  
+  add() {
+    var name, i;
 
-  this._onchange = onchange
+    for (i = 0; i < arguments.length; i += 1) {
+      name = '' + arguments[i];
 
-  this.reset(className)
-}
+      if (this.indexOf(name) !== -1) {
+        continue;
+      }
 
-ClassList.prototype.reset = className => {
-  var classes = trim(className).split(/\s+/),
-    i
-  this.length = 0
+      this.push(name);
+    }
 
-  for (i = 0; i < classes.length; i += 1) {
-    if (classes[i])
-      arr.push.call(this, classes[i])
+    this.onchange(this.toString());
+
+    return this;
+  }
+  
+  remove() {
+    var index,
+        name,
+        i;
+
+    for (i = 0; i < arguments.length; i += 1) {
+      name = arguments[i] + '';
+      index = this.indexOf(name);
+
+      if (index < 0) {
+        continue;
+      }
+
+      this.splice(index, 1);
+    }
+
+    this.onchange(this.toString());
+
+    return this;
+  }
+  
+  contains(name) {
+    name += '';
+    console.log('contains classes', Array.from(this), name);
+    return this.indexOf(name) !== -1;
+  }
+  
+  toggle(name, force) {
+    name += '';
+
+    if (force === true) {
+      return this.add(name);
+    }
+    if (force === false) {
+      return this.remove(name);
+    }
+
+    return this[this.contains(name) ? 'remove' : 'add'](name);
+  }
+  
+  toString() {
+    return this.join(' ');
   }
 }
 
-/**
- * add(class1 [, class2 [, ...]]) adds the given class(es) to the
- * element.
- *
- * @param {String} ...
- * @return {Context}
- */
-ClassList.prototype.add = function () {
-  var name,
-      i
-
-  for (i = 0; i < arguments.length; i += 1) {
-    name = '' + arguments[i]
-
-    if (indexOf(this, name) >= 0)
-      continue
-
-    arr.push.call(this, name)
-  }
-
-  this._onchange(this.toString())
-
-  return this
-}
-
-/**
- * remove(class1 [, class2 [, ...]]) removes the given class(es) from
- * the element.
- *
- * @param {String} ...
- * @return {Context}
- */
-ClassList.prototype.remove = function () {
-  var index,
-      name,
-      i
-
-  for (i = 0; i < arguments.length; i += 1) {
-    name = '' + arguments[i]
-    index = indexOf(this, name)
-
-    if (index < 0) continue
-
-    arr.splice.call(this, index, 1)
-  }
-
-  this._onchange(this.toString())
-
-  return this
-}
-
-/**
- * contains(name) determines if the element has a given class.
- *
- * @param {String} name
- * @return {Boolean}
- */
-ClassList.prototype.contains = function (name) {
-  name += ''
-  return indexOf(this, name) >= 0
-}
-
-/**
- * toggle(name [, force]) toggles a class. If force is a boolean,
- * this method is basically just an alias for add/remove.
- *
- * @param {String} name
- * @param {Boolean} force
- * @return {Context}
- */
-ClassList.prototype.toggle = function (name, force) {
-  name += ''
-
-  if (force === true) return this.add(name)
-  if (force === false) return this.remove(name)
-
-  return this[this.contains(name) ? 'remove' : 'add'](name)
-}
-
-/**
- * toString() returns the className of the element.
- *
- * @return {String}
- */
-ClassList.prototype.toString = function () {
-  return arr.join.call(this, ' ')
-}
+module.exports = ClassList;
